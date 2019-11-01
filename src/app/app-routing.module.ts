3183b6e88882cf776885ef02c914af8c4ccaad81
owinolawrence <owinolawrence21@gmail.com>
2019-11-01 12:51:59 +0300

    create component files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { ProfileComponent } from './profile/profile.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent} from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:"about", component:AboutComponent},
+  {path:"home", component:HomeComponent},
+  {path:"profile",component:ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    NavbarComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,
