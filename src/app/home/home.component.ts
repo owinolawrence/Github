6038e86b8f4dd02ef3b4d2b48ad82d30db793f7a
owinolawrence <owinolawrence21@gmail.com>
2019-11-02 22:33:43 +0300

    insert parent compent link

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { GithubService } from './../git-service/github.service';
+import { Github } from './../github';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+ github:Github[];
+  constructor(githubService:GithubService) { 
+    this.github=githubService.getGithub()
+  }
 
   ngOnInit() {
   }
