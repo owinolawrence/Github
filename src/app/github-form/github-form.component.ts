c64dd03b5aa7c23a225fce93cf6d815e4c20fcf9
owinolawrence <owinolawrence21@gmail.com>
2019-11-06 08:21:04 +0300

    create searchTerm properties

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-github-form',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-form.component.css']
 })
 export class GithubFormComponent implements OnInit {
-
+  @Output() emitSearch = new EventEmitter<any>()
+  searchTerm:string;
   constructor() { }
+  search(){
+    this.emitSearch.emit(this.searchTerm)
+  }
+
+ 
 
   ngOnInit() {
   }
