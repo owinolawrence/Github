7514b1670b0be3e1b98da83483d4c1ccddcf5574
owinolawrence <owinolawrence21@gmail.com>
2019-11-06 08:28:39 +0300

    delete certain properties

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,13 +1,14 @@
 export class User {
     constructor(
-        public avatar_url:string,
+        
         public name: string,
-        public email: string,
-        public location: string,
-        public repo: string,
-        public created:number,
-        public followers:number,
-        public followings:number,
+        public avatar_url:string,
+        // public email: string,
+        // public location: string,
+        // public repos: number,
+        // public created:number,
+        // public followers:number,
+        // public followings:number,
 
      
     ){}
